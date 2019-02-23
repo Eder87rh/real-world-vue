8ea2d79f52e957846ca6295ff1500885186ee78a
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2019-02-22 20:25:55 -0600

    Dynamic routing & history mode

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>Evento list</h1>
+  <div>
+    <h1>Evento list</h1>
+
+    <router-link :to="{ name: 'event-show', params: { id: '1' } }">
+      Show Event #1
+    </router-link>
+  </div>
 </template>
